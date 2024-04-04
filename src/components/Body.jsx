46c13409e5d2aae91209c17aed4c39751bc0c6db
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-04 19:19:42 +0530

    Initial commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Restaurant from "./Restaurant";
+
+const Body = () => {
+  return (
+    <>
+      <div className="p-10">
+        <Restaurant />
+      </div>
+    </>
+  );
+};
+
+export default Body;
---
+++ src/components/Restaurant.jsx
@@ -0,0 +1,18 @@
+const Restaurant = () => {
+    return(
+        <>
+            <div className="restaurant-card flex flex-col box-content border-4 rounded-lg shadow-lg w-64 h-72 p-4">
+                <img className="w-full h-1/2 rounded-md" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/535fc9f9c135f7982317bbb6a64a1ffc" alt="restaurant_image" />
+                <div>
+                    <p>RES_NAME</p>
+                    <p>Ratings</p>
+                    <p>Delivery Time</p>
+                    <p>Cusines</p>
+                    <p>res address</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Restaurant;
