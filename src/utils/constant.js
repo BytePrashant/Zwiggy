e94e7b26aa291adf2f8463a0a383ecd4ce58c911
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-12 19:46:48 +0530

    added utility folder

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL = "https://flowbite.com/docs/images/logo.svg";
+
+export const Swiggy_Restaurant_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
