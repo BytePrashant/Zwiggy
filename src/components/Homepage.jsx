1c7f19759e9667ad935b9a840d3fc79f6b24cd00
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-15 17:12:24 +0530

    added search functionality

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,6 +5,8 @@ import Skeleton from "./Skeleton";
 
 const Homepage = () => {
   const [restaurantsList, setRestaurantsList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRes, setFilteredRes] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -17,18 +19,20 @@ const Homepage = () => {
       jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
     );
+
+    setFilteredRes(
+      jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
-  console.log("****")
-  if (restaurantsList.length === 0) {  //<- this right here is condition rendering we are passing condition to render
+  if (restaurantsList.length === 0) {
     let skeleton = [];
-    for(var i=0; i<20; i++){
-       skeleton.push(<Skeleton key={i} />);
+    for (var i = 0; i < 20; i++) {
+      skeleton.push(<Skeleton key={i} />);
     }
     return (
       <div className="py-20 mx-14">
-        <div className="flex flex-wrap justify-start gap-4">
-          {skeleton}
-        </div>
+        <div className="flex flex-wrap justify-start gap-4">{skeleton}</div>
       </div>
     );
   }
@@ -36,11 +40,45 @@ const Homepage = () => {
   return (
     <div>
       <div className="py-20 mx-14">
+        <div className="border-2 my-2 inline-block px-2 py-1 mr-2">
+          <input
+            className="search-box"
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                const filtered = restaurantsList.filter((res) => {
+                  return res.info.name
+                    .toLowerCase()
+                    .includes(searchText.toLowerCase());
+                });
+                setFilteredRes(filtered);
+              }
+            }}
+          />
+          <button
+            onClick={() => {
+              const filtered = restaurantsList.filter((res) => {
+                return res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              setFilteredRes(filtered);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <div className="border-2 my-2 inline-block px-2 py-1">
           <button
             className="filter-btn"
             onClick={() => {
-              const filtered = restaurantsList.filter((item) => item.info.avgRating >= 4);
+              const filtered = restaurantsList.filter(
+                (item) => item.info.avgRating >= 4
+              );
               setRestaurantsList(filtered);
             }}
           >
@@ -48,7 +86,7 @@ const Homepage = () => {
           </button>
         </div>
         <div className="flex flex-wrap justify-start gap-4">
-          {restaurantsList.map((items) => (
+          {filteredRes.map((items) => (
             <RestaurantCard key={items.info.id} resData={items.info} />
           ))}
         </div>
