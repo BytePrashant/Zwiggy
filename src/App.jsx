927bfca630280ff8459d1f6ca6c6c89838472c1e
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-16 23:47:45 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </Router>
+      <Outlet />
+    </div>
   );
 }
 
-export default App;
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default appRouter;
