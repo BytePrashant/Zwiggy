57928777f4403fb72c73571bcd3f034d0121af2e
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-18 23:49:16 +0530

    Restaurant menu created

---
+++ src/components/RestaurantMenu.jsx
@@ -0,0 +1,18 @@
+const RestaurantMenu = () => {
+    return (
+        <div classname="">
+            <h1>Restaurant Name</h1>
+            <p>Menu</p>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                <li>Item 5</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default RestaurantMenu;
