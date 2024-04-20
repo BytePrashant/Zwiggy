b3be99a8fb9791c75c333b4ba1e73a3dd29b22c9
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-21 01:21:52 +0530

    added Restaurant menu URL

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,6 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://flowbite.com/docs/images/logo.svg";
 
-export const Swiggy_Restaurant_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_RESTAURANT_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+export const RESTAURANT_MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.4969&lng=80.3246&restaurantId="
