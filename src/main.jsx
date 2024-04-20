4c7212d888c8a4f9656087e0644322afcf459306
Prashant Tomar <prashanttomar765@gmail.com>
2024-04-21 01:21:08 +0530

    added RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import AppRouter from './AppLayout.jsx'
+import AppRouter from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
