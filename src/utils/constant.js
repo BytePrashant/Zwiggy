5aacbac175cf424c3a8d5a50c48cc83a990200f3
Prashant Tomar <prashanttomar765@gmail.com>
2024-05-07 15:09:31 +0530

    URL corrected

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,6 +2,6 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_l
 
 export const LOGO_URL = "https://flowbite.com/docs/images/logo.svg";
 
-export const SWIGGY_RESTAURANT_API = "https://zwiggy-0m5u.onrender.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const SWIGGY_RESTAURANT_API = "https://zwiggy-0m5u.onrender.com/api/restaurants/?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const RESTAURANT_MENU_API = "https://zwiggy-0m5u.onrender.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.4969&lng=80.3246&restaurantId="    
+export const RESTAURANT_MENU_API = "https://zwiggy-0m5u.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=26.4969&lng=80.3246&restaurantId="    
